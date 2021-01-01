dc413b16ffc4f9f3a587dd9f581706af9edf0a14
linxueyuanstdio <761516186@qq.com>
2021-01-02 00:31:23 +0800

    svave

--- app/src/main/java/com/same/ui/MainActivity.java
+++ app/src/main/java/com/same/ui/MainActivity.java
@@ -2,12 +2,52 @@ package com.same.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.timecat.identity.readonly.RouterHub;
+import com.xiaojinzi.component.impl.Callback;
+import com.xiaojinzi.component.impl.Router;
+import com.xiaojinzi.component.impl.RouterErrorResult;
+import com.xiaojinzi.component.impl.RouterRequest;
+import com.xiaojinzi.component.impl.RouterResult;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button button = new Button(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Router.with().hostAndPath(RouterHub.LOGIN_LoginActivity)
+                      .forward(new Callback() {
+                          @Override
+                          public void onSuccess(@NonNull RouterResult result) {
+                          }
+
+                          @Override
+                          public void onEvent(@Nullable RouterResult successResult, @Nullable RouterErrorResult errorResult) {
+                          }
+
+                          @Override
+                          public void onCancel(@Nullable RouterRequest originalRequest) {
+
+                          }
+
+                          @Override
+                          public void onError(@NonNull RouterErrorResult errorResult) {
+                              Log.e("ui", errorResult.getError().toString());
+                          }
+                      });
+            }
+        });
+setContentView(button);
     }
 
 }
