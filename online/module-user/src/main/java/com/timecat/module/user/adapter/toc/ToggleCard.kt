0fa67601fffc96dc588aa59ce0510623e286b23d
linxueyuanstdio <761516186@qq.com>
2021-08-11 12:32:58 +0800

    save

---
+++ online/module-user/src/main/java/com/timecat/module/user/adapter/toc/ToggleCard.kt
@@ -0,0 +1,11 @@
+package com.timecat.module.user.adapter.toc
+
+/**
+ * @author 林学渊
+ * @email linxy59@mail2.sysu.edu.cn
+ * @date 2021/8/10
+ * @description null
+ * @usage null
+ */
+class ToggleCard {
+}
